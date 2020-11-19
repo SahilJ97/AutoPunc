ae334e621461d78c0c2b0e25ce46f2ed9a1035d6
Sahil Jayaram <sahil@Sahils-MacBook-Pro.local>
2020-11-19 09:43:02 -0800

    demo

--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,20 @@
 from src import data
+import torch
 
 if __name__ == "__main__":
-    print()
-    # use data.get_punctuated_strings() on a test file.
+    device = "cpu"
+
+    data_dir = "../data/demo/"  # a punctuation-free datafile
+    model = torch.load("model.pt", map_location=device)
+    demo_set = data.AutoPuncDataset(data_dir)
+
+    predictions = []
+    for speech, _ in demo_set.get_speeches():
+        speech["tokens"] = speech["tokens"].to(device)
+        speech["pros_feat"] = speech["pros_feat"].to(device)
+
+        predictions.append(model.predict(speech))
+
+    for punctuated_string in data.get_punctuated_strings(demo_set, predictions):
+        print(punctuated_string)
+        print()
